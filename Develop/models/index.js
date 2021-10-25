f5df6729c1e1b54bbefd4633d2d729e1b7abac3f
JoeyGitsIt <josephperri3@gmail.com>
2021-10-25 17:07:51 -0400

    added model data to .js files in models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,51 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(
+  Category,
+  // this is sus might need to make sure there is a foreign key???
+  // { foreignKey: "category_id" },
+  {
+    through: {
+      model: Category,
+      unique: false,
+    },
+
+    as: "product_category",
+  }
+);
 
 // Categories have many Products
+Category.hasMany(Product, {
+  through: {
+    model: Product,
+    unique: false,
+  },
+
+  as: "categories_of_products",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: Tag,
+    unique: false,
+  },
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: Product,
+    unique: false,
+  },
+  as: "tags_of_products",
+});
 
 module.exports = {
   Product,
